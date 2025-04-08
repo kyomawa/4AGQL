67ea54fbed1f4c0655964648a560bce58d81149f
kyomawa <kyomawa@protonmail.com>
2025-04-08 07:59:17 +0200

    feat(classes-service): add fn to get classes by user_id.

--- classes-service/src/query.rs
+++ classes-service/src/query.rs
@@ -1,5 +1,6 @@
 use crate::schema::Class;
 use async_graphql::*;
+use common::{jwt::jwt_utils::Claims, schemas::AuthRole};
 use futures_util::TryStreamExt;
 use mongodb::{
     Database,
@@ -44,4 +45,53 @@ impl QueryRoot {
             .ok_or("No class with this id was found")?;
         Ok(class)
     }
+
+    // =============================================================================================================================
+
+    async fn get_classes_by_user_id(
+        &self,
+        ctx: &Context<'_>,
+        user_id: String,
+    ) -> Result<Vec<Class>> {
+        let token = ctx
+            .data_opt::<Claims>()
+            .ok_or("Unauthorized: token missing or invalid")?;
+
+        let requested_user_id = ObjectId::parse_str(&user_id)?;
+
+        let filter = match token.role {
+            AuthRole::Admin => {
+                doc! {
+                    "$or": [
+                        { "creator_id": requested_user_id },
+                        { "user_ids": { "$in": [requested_user_id] } }
+                    ]
+                }
+            }
+            AuthRole::User => {
+                let connected_user = ObjectId::parse_str(&token.user_id)?;
+                if requested_user_id != connected_user {
+                    return Err("Unauthorized: You can only view your own classes".into());
+                }
+                doc! { "user_ids": { "$in": [requested_user_id] } }
+            }
+            AuthRole::Professor => {
+                let connected_user = ObjectId::parse_str(&token.user_id)?;
+                if requested_user_id != connected_user {
+                    return Err("Unauthorized: You can only view your own classes".into());
+                }
+                doc! { "creator_id": connected_user }
+            }
+        };
+
+        let db = ctx.data_unchecked::<Database>();
+        let collection = db.collection::<Class>("classes");
+
+        let cursor = collection.find(filter).await?;
+        let classes = cursor.try_collect().await?;
+
+        Ok(classes)
+    }
+
+    // =============================================================================================================================
 }
